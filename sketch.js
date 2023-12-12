75954eca6ea4a3fafe625805f709a936e1a85222
michelle.duong <michelle.duong@study.hs-duesseldorf.de>
2023-12-12 23:25:00 +0100

    sdf

--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@ let cx, cy;
 let bg;
 const particles = [];
 let dragPoint = null;
-const numParticles = 25;
+const numParticles = 75;
 const dragRadius = 110;
-let animationSpeed = 35; // Anfangsgeschwindigkeit (kann angepasst werden)
+let animationSpeed = 15; // Anfangsgeschwindigkeit (kann angepasst werden)
 
 function setup() {
  
@@ -130,8 +130,8 @@ class Particle {
     // Move particles based on device motion
     const dx = constrain(rotationY, -1, 1);
     const dy = constrain(rotationX, -1, 1);
-    this.pos.x += dx * 2;
-    this.pos.y += dy * 2;
+    this.pos.x += dx * 0.90;
+    this.pos.y += dy * 0.90;
 
     this.pos.add(p5.Vector.mult(this.vel, speed)); // Use speed to control the velocity
     this.edges();
@@ -169,10 +169,10 @@ class Particle {
         return; // Skip drawing if particles are too close
       }
      
-      if (d < 200) {
+      if (d < 190) {
         stroke(this.color);
         //strokeWeight(3);
-        strokeWeight(random(7.5,13));
+        strokeWeight(random(7.5,11));
         // Erzeuge squiggly Line mithilfe von Perlin-Noise
         beginShape();
         for (let t = -0.005; t <= 1; t += 0.125) {
