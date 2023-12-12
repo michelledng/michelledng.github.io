fac8932339341fd3cc256617bd48b7f4520bf39f
michelle.duong <michelle.duong@study.hs-duesseldorf.de>
2023-12-12 23:19:01 +0100

    sdf

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let cx, cy;
 let bg;
 const particles = [];
 let dragPoint = null;
-const numParticles = 80;
+const numParticles = 60;
 const dragRadius = 110;
 let animationSpeed = 15; // Anfangsgeschwindigkeit (kann angepasst werden)
 
@@ -169,7 +169,7 @@ class Particle {
         return; // Skip drawing if particles are too close
       }
      
-      if (d < 260) {
+      if (d < 200) {
         stroke(this.color);
         //strokeWeight(3);
         strokeWeight(random(7.5,11));
