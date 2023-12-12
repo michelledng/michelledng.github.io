4989c547d4bd08f90b3a861cb896c93e3dbfe8b8
michelle.duong <michelle.duong@study.hs-duesseldorf.de>
2023-12-12 23:26:39 +0100

    sdf

--- sketch.js
+++ sketch.js
@@ -169,7 +169,7 @@ class Particle {
         return; // Skip drawing if particles are too close
       }
      
-      if (d < 180) {
+      if (d < 250) {
         stroke(this.color);
         //strokeWeight(3);
         strokeWeight(random(7.5,11));
