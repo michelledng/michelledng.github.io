e8f02c09f37272daf8b9c608853430c8743db631
michelle.duong <michelle.duong@study.hs-duesseldorf.de>
2023-12-12 18:42:20 +0100

    sadfasd

--- sketch.js
+++ sketch.js
@@ -169,10 +169,10 @@ class Particle {
         return; // Skip drawing if particles are too close
       }
      
-      if (d < 120) {
+      if (d < 170) {
         stroke(this.color);
         //strokeWeight(3);
-        strokeWeight(random(14,15));
+        strokeWeight(random(12,13));
         // Erzeuge squiggly Line mithilfe von Perlin-Noise
         beginShape();
         for (let t = -0.005; t <= 1; t += 0.125) {
