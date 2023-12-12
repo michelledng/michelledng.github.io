129cf117fa2fd0307279df5743cd042ce88185d9
michelle.duong <michelle.duong@study.hs-duesseldorf.de>
2023-12-12 23:29:15 +0100

    asfdf

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let cx, cy;
 let bg;
 const particles = [];
 let dragPoint = null;
-const numParticles = 60;
+const numParticles = 40;
 const dragRadius = 110;
 let animationSpeed = 15; // Anfangsgeschwindigkeit (kann angepasst werden)
 
