bc9a38db87a0e5523ea1387c62461ee1bc60fc00
michelle.duong <michelle.duong@study.hs-duesseldorf.de>
2023-12-13 00:10:13 +0100

    asd

--- sketch.js
+++ sketch.js
@@ -172,7 +172,7 @@ class Particle {
       if (d < 205) {
         stroke(this.color);
         //strokeWeight(3);
-        strokeWeight(random(7,10));
+        strokeWeight(random(7,15));
         // Erzeuge squiggly Line mithilfe von Perlin-Noise
         beginShape();
         for (let t = -0.005; t <= 1; t += 0.125) {
